41bb389d901b0e31836d48235e358a4e4e2eee8b
Heidy Pastor <heidypastor@gmail.com>
2022-11-23 16:41:45 -0500

    Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Garden in love Flat Toronto',
+  address: '20 Laiton Gardens Toronto W9 1DT',
+  description: 'One double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Dann Carlton Flat Quebec',
+  address: '35 rue of quenn Quebec W9 1DT',
+  description: 'four double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 80,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Caribe Flat Cartagena',
+  address: '390 rue of bocagrande Columbia W9 1DT',
+  description: 'five double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 6
+)
